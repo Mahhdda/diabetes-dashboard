30fa8cd6e95e824867dde3503bcdf647857388d9
Mahhdda <mahhdda@gmail.com>
2025-08-18 17:37:29 +0330

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import StandardScaler, SimpleImputer
+from sklearn.preprocessing import StandardScaler, SimpleImputer  
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, classification_report
 from sklearn.linear_model import LogisticRegression
