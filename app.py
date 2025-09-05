1d5297622a9c7e6de2cda8538e4a62faab5f57ec
Mahhdda <mahhdda@gmail.com>
2025-09-05 19:26:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def update_page(overview_clicks, eda_clicks, advanced_clicks, models_clicks, pre
             - ایجاد داشبورد برای پیش‌بینی دیابت و پیشنهاد برنامه‌های شخصی‌سازی‌شده بر اساس ویژگی‌های کلیدی.
             این داشبورد با Dash ساخته شده و روی Render deploy می‌شود.
             """, style={'direction': 'rtl', 'text-align': 'right', 'fontSize': '18px', 'margin': '20px'}),
-            html.Img(src='/img/img1.png', style={'width': '50%', 'margin': '20px auto', 'display': 'block'}, alt="تصویر داشبورد"),
+            html.Img(src='../img/img1.png', style={'width': '50%', 'margin': '20px auto', 'display': 'block'}, alt="تصویر داشبورد"),
             html.P("در صورت لود نشدن تصویر، لطفاً مطمئن شوید که فایل img1.png در پوشه img قرار دارد.", style={'direction': 'rtl', 'text-align': 'right', 'fontSize': '16px', 'color': '#FF0000', 'margin': '10px'}),
             html.P("""
             مزایا و کاربردهای داشبورد:
@@ -498,3 +498,4 @@ if __name__ == '__main__':
 application = app.server  # این خط شیء WSGI رو از Dash می‌سازه
 if __name__ == '__main__':
     app.run_server(debug=True, host='127.0.0.1', port=8000)
+
